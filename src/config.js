f984e6f4094302e3a1e9d2dbc54246336dd2876d
daoliu1221 <141800732+daoliu1221@users.noreply.github.com>
2024-04-05 03:34:23 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const CONFIG = {
     'whiteboard_id': '09dba888a0c5beb7cadcb74ac8ffe96c52197df0de63eac2da86f6ea12c86fa5',
     'title': '稻流Daoliu', // 站点标题
     'pages': {
-        'Action': 'bc675d03-0479-4f07-999a-a8c4f60a166c',
-        'Archive': '87247526-363e-4ce0-9a62-49d6b5413650',
+        'Action': '87247526-363e-4ce0-9a62-49d6b5413650',
+        'Archive': 'bc675d03-0479-4f07-999a-a8c4f60a166c',
         'About': '6cca6d23-f99d-43b0-ae17-a55475571a3c'
     }
 }
